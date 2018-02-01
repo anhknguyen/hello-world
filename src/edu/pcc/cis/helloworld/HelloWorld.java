3bf5e358d7d5547f6525fc437a99b155677e188a
Anh Nguyen <anh.nguyen46@pcc.edu>
2018-01-31 16:36:13 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,13 @@
 package edu.pcc.cis.helloworld;
-
+/*
+ * Lab4
+ * @author Anh Nguyen
+ * @version 1/31/2018
+ * Created HelloWorld class
+ * Print out welcome message and my name
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
