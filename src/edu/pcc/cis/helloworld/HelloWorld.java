a37731a5f0647df89009960924f396655d7668d5
Anh Nguyen <anh.nguyen46@pcc.edu>
2018-01-31 16:52:12 -0800

    print out the author name-Anh Nguyen

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -9,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("My name is Anh Nguyen");
     }
 }
